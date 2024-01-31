b27af999a2bbe67c95c7cd1c4e8a44ce615c31a5
Victoria <vicoliveira116@gmail.com>
2024-01-31 18:43:40 -0300

    'Atualizando'

---
+++ Array/exemplo3.js
@@ -0,0 +1,13 @@
+/* O FOR ele é usado para percorrer uma quantidade específica de vezes
+let index = 0; - essa parte você está declarando a variável de controle
+index < .larrayength; - essa parte é a estrutura condicional que vai dizer se entra ou não no FOR
+index++ - essa parte é o que vai acontecer depois de terminar de executar o FOR 
+ela inteira é assim for (let index = 0; index < .larrayength; index++) 
+i++ é a mesma coisa que i = i + i*/
+
+
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
---
+++ Array/exemplo4.js
@@ -0,0 +1,16 @@
+/* O FOR ele é usado para percorrer uma quantidade específica de vezes
+let index = 0; - essa parte você está declarando a variável de controle
+index < .larrayength; - essa parte é a estrutura condicional que vai dizer se entra ou não no FOR
+index++ - essa parte é o que vai acontecer depois de terminar de executar o FOR 
+ela inteira é assim for (let index = 0; index < .larrayength; index++) 
+i++ é a mesma coisa que i = i + i
+nome.length; vai verificar o tamanho do meu texto*/
+
+
+
+
+const nome = "Victoria Oliveira"
+
+for (let i = 0; i < nome.length; i++) {
+    console.log(nome[i]);
+}
